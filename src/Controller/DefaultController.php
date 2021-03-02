432119ebd9cb333dd123481621d76f25fa2cc283
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-03-02 17:27:23 +0100

    adding logger ip info

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -13,10 +15,43 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      * @param SerializerInterface $serializer
+     * @param LoggerInterface $logger
+     * @param Request $request
      * @return Response
      */
-    public function index(SerializerInterface $serializer): Response
+    public function index(SerializerInterface $serializer, LoggerInterface $logger,
+    Request $request): Response
     {
+        $clientIP = $request->getClientIp();
+        $logger->info('Request vers / ', [
+            'client_ip' => $clientIP,
+        ]);
+
+        $code = Response::HTTP_FORBIDDEN;
+        $result = [
+            "message" => "Operation not allowed"
+        ];
+
+        $jsonResult = $serializer->serialize($result, 'json');
+        return new JsonResponse($jsonResult, $code,
+            ['Access-Control-Allow-Origin' => 'http://localhost'], true);  //CROSS ORIGIN
+    }
+
+    /**
+     * @Route("/api", name="api")
+     * @param SerializerInterface $serializer
+     * @param LoggerInterface $logger
+     * @param Request $request
+     * @return Response
+     */
+    public function api(SerializerInterface $serializer, LoggerInterface $logger,
+                          Request $request): Response
+    {
+        $clientIP = $request->getClientIp();
+        $logger->info('Request vers /api/ ', [
+            'client_ip' => $clientIP,
+        ]);
+
         $code = Response::HTTP_FORBIDDEN;
         $result = [
             "message" => "Operation not allowed"
--- src/Controller/EtatController.php
+++ src/Controller/EtatController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\EtatRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -12,13 +14,22 @@ use Symfony\Component\Serializer\SerializerInterface;
 class EtatController extends AbstractController
 {
     /**
-     * @Route("/etat", name="etat", methods={"GET"})
+     * @Route("api/etat", name="get_all_etat", methods={"GET"})
      * @param EtatRepository $etatRepository
      * @param SerializerInterface $serializer
+     * @param LoggerInterface $logger
+     * @param Request $request
      * @return Response
      */
-    public function index(EtatRepository $etatRepository, SerializerInterface $serializer): Response
+    public function getAllEtat(EtatRepository $etatRepository, SerializerInterface $serializer,
+                          LoggerInterface $logger, Request $request): Response
     {
+
+        $clientIP = $request->getClientIp();
+        $logger->info('Request vers api/etat/ ', [
+            'client_ip' => $clientIP,
+        ]);
+
         $etat = $etatRepository->findAll();
         $jsonEtat = $serializer->serialize($etat, 'json');
 
